0fb8cc6073a4c65dbdc4da55859f79690705e419
ikkromm18 <muhammadikkrom749@gmail.com>
2025-04-03 03:58:36 +0700

    feat:section sejarah, visi, misi, program di profil

---
+++ resources/views/components/profil/program.blade.php
@@ -0,0 +1,12 @@
+<section class=" dark:bg-gray-900 py-20">
+    <div class="mx-auto max-w-screen-xl flex flex-col rounded-md">
+
+
+        <h1 class="font-bold text-2xl px-2 md:px-0 md:text-4xl text-center mb-2">PROGRAM IPNU KOTA SANTRI</h1>
+
+
+
+
+    </div>
+
+</section>
---
+++ resources/views/components/profil/visi.blade.php
@@ -0,0 +1,18 @@
+<section class=" dark:bg-gray-900 pt-20">
+    <div class="mx-auto max-w-screen-xl flex flex-col rounded-md">
+
+
+        <h1 class="font-bold text-2xl px-2 md:px-0 md:text-4xl text-center mb-2">VISI IPNU</h1>
+
+        <p class="text-center font-normal text-md mt-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt
+            ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum."</p>
+
+
+    </div>
+
+</section>
